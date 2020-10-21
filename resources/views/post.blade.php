2a4ac21f434d9c81c01ddf3623f57c673e763cfb
gyflorand <gyflorand@gmail.com>
2020-10-22 00:34:17 +0200

    Laravel From Scratch - Section 4/Views. Layout Pages

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,8 @@
-<!doctype html>
 <?php
+/** @var \App\Models\Post $post */ ?>
 
-/** @var \App\Models\Post $post */
+@extends('layout')
 
-?>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Posts</title>
-    </head>
-    <body>
-        <h1>{{ $post->body }}</h1>
-    </body>
-</html>
+@section('content')
+    <h1>{{ $post->body }}</h1>
+@endsection
