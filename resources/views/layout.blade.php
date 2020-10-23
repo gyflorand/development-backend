2e5d8669a76f6778576364e724f78e38245bbf07
gyflorand <gyflorand@gmail.com>
2020-10-23 16:05:32 +0200

    Laravel From Scratch - Section 4/Views. Set an Active Menu Link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
       lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <title></title>
+        <title>Development</title>
         <meta name="keywords" content=""/>
         <meta name="description" content=""/>
         <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet"/>
@@ -18,16 +18,31 @@
                 </div>
                 <div id="menu">
                     <ul>
-                        <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-                        <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-                        <li><a href="/about" accesskey="3" title="">About Us</a></li>
-                        <li><a href="#" accesskey="4" title="">Careers</a></li>
-                        <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+                        <li class={{ Request::is('/') ? 'current_page_item' : '' }}>
+                            <a href="/" accesskey="1" title="">Homepage</a>
+                        </li>
+                        <li class={{ Request::is('/clients') ? 'current_page_item' : '' }}>
+                            <a href="/clients" accesskey="2" title="">Our Clients</a>
+                        </li>
+                        <li class={{ Request::is('about') ? 'current_page_item' : '' }} >
+                            <a href="/about" accesskey="3" title="">About Us</a>
+                        </li>
+                        <li class={{ Request::is('/careers') ? 'current_page_item' : '' }}>
+                            <a href="/careers" accesskey="4" title="">Careers</a>
+                        </li>
+                        <li class={{ Request::is('/contact') ? 'current_page_item' : '' }}>
+                            <a href="/contact" accesskey="5" title="">Contact Us</a>
+                        </li>
                     </ul>
                 </div>
             </div>
             @yield('header')
         </div>
         @yield('content')
+
+        <div id="copyright" class="container">
+            <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by
+                <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        </div>
     </body>
 </html>
