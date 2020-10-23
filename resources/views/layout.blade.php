a3bddf45d008dfe1f11f073726283a684a0c8d39
gyflorand <gyflorand@gmail.com>
2020-10-23 19:13:28 +0200

    Laravel From Scratch - Section 4/Views. Asset Compilation with Laravel Mix and webpack

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,6 +9,7 @@
         <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet"/>
         <link href="/css/default.css" rel="stylesheet" type="text/css" media="all"/>
         <link href="/css/fonts.css" rel="stylesheet" type="text/css" media="all"/>
+        <link rel="stylesheet" href="css/app.css">
     </head>
     <body>
         <div id="header-wrapper">
@@ -44,5 +45,7 @@
             <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by
                 <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
         </div>
+
+        <script src="/js/app.js"></script>
     </body>
 </html>
