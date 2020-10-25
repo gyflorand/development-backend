2f90396d679e4b3d82a4c3a2659bcc3fddddb2d8
gyflorand <gyflorand@gmail.com>
2020-10-26 00:56:28 +0100

    Laravel From Scratch - Section 5/Forms. The Seven Restful Controller Actions

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -9,6 +9,8 @@ class ArticlesController extends Controller
 {
     public function index(): View
     {
+        // Render a list of a resource
+
         $articles = Article::latest('created_at')->get();
 
         return view('articles.index', ['articles' => $articles]);
@@ -16,8 +18,35 @@ class ArticlesController extends Controller
 
     public function show(int $id): View
     {
+        // Show a single resource
+
         $article = Article::find($id);
 
         return view('articles.show', ['article' => $article]);
+    }
+
+    public function create()
+    {
+        // Shows a view to create a new resource
+    }
+
+    public function store()
+    {
+        // Persist the new resource
+    }
+
+    public function edit()
+    {
+        // Show a view to edit an existing resource
+    }
+
+    public function update()
+    {
+        // Persist the edited resource
+    }
+
+    public function destroy()
+    {
+        // Delete the resource
     }
 }
